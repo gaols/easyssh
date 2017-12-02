7f4890401ce1bdcbbd3c59a9be2864ae51b3e823
gaols <igaols@foxmail.com>
2017-12-02 14:24:17 +0800

    shell utils for tar and untar

--- shellutils.go
+++ shellutils.go
@@ -3,6 +3,8 @@ package easyssh
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
+	"errors"
 )
 
 // https://studygolang.com/articles/4004   <- run shell command and read output line by line
@@ -13,4 +15,30 @@ func Local(localCmd string, paras ...interface{}) (out string, err error) {
 	ret, err := cmd.Output()
 	out = string(ret)
 	return
+}
+
+// Tar pack the targetPath and put tarball to tgzPath, targetPath and tgzPath should both the absolute path.
+func Tar(tgzPath, targetPath string) error {
+	if !IsDir(targetPath) && !IsRegular(targetPath) {
+		return errors.New("invalid pack path: " + targetPath)
+	}
+
+	targetPathDir := filepath.Dir(RemoveTrailingSlash(targetPath))
+	target := filepath.Base(RemoveTrailingSlash(targetPath))
+	_, err := Local("tar czf %s -C %s %s", tgzPath, targetPathDir, target)
+	return err
+}
+
+// UnTar unpack the tarball specified by tgzPath and extract it to the path specified by targetPath
+func UnTar(tgzPath, targetPath string) error {
+	if !IsDir(targetPath) {
+		return errors.New("tar extract path invalid: " + targetPath)
+	}
+
+	if !IsRegular(tgzPath) {
+		return errors.New("tar path invalid: " + tgzPath)
+	}
+
+	_, err := Local("tar xf %s -C %s", tgzPath, targetPath)
+	return err
 }
